73d5e5c15623c87e5a494d63be8f4da833b277f4
Sukrit Chanachaimongkonkun <tonwan.sukrit@gmail.com>
2024-05-17 23:36:18 +0700

    update code

--- netpie.js
+++ netpie.js
@@ -1,21 +1,63 @@
 var x = 0;
+let sensors= {
+    temp:"",
+    humi:"",
+    dust:"",
+}
 let on = "off";
 
-client = new Paho.MQTT.Client("mqtt.netpie.io",443,"xxx"); // The last one is Client ID
+client = new Paho.MQTT.Client("mqtt.netpie.io",443,"87050973-a747-4a1a-a0d2-5822f372db"); // The last one is Client ID
 client.onMessageArrived = onMessageArrived;
 
 var options = {
     useSSL: true,
-    userName: "xxx", // This is token
-    password: "xxx", // This is secret
+    userName: "MFkDHUkm86b5PBQ6x8Ejazq4cVNWnQ", // This is token
+    password: "WM8Zs6ozJGzsJYxFc1WUkPRquvQRMc", // This is secret
     onSuccess: onConnect,
     onFailure: doFail,                                                  
 }
 
 client.connect(options);
 
+//have to both send msg and update the shadow
+
 function onConnect(){
     client.subscribe("@msg/test");
+}
+
+async function getShadowData() {
+  let fdata;
+  const url = "https://api.netpie.io/v2/device/shadow/data"; 
+  const clientID = "96df986a-def6-4028-9794-637a355c7a90"; // clientId of sensors
+  const token = "squtuLxvW749hzwJhXNzL9PULrasxuqE"; // token of sensors
+
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Authorization": `Basic ${btoa(`${clientID}:${token}`)}`
+    }
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    console.log("Shadow Data:", data);
+    // return data;
+    fdata = data;
+  } else {
+    console.error("Failed to retrieve shadow data", response.statusText);
+  }
+  sensors = fdata;
+}
+
+// getShadowData();
+// refresh(); 
+
+function refresh(){
+    getShadowData();
+    console.log(sensors);
+    document.getElementById("temp").innerHTML = "temp = " + sensors.data.temp;
+    document.getElementById("humi").innerHTML = "humi = " + sensors.data.humi;
+    document.getElementById("dust").innerHTML = "dust = " + sensors.data.dust;
 }
 
 function doFail(e){
@@ -55,7 +97,7 @@ var mqttSend = function (topic,msg){
 
 
 
-
+/* */
 
 
 
