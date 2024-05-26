f4f5fe9d5bcb22e349b68075ddcbed3e25c2f3c3
Sukrit Chanachaimongkonkun <tonwan.sukrit@gmail.com>
2024-05-26 23:10:38 +0700

    push all updated code

--- netpie.js
+++ netpie.js
@@ -6,23 +6,28 @@ let sensors= {
 }
 let on = "off";
 
-client = new Paho.MQTT.Client("mqtt.netpie.io",443,"87050973-a747-4a1a-a0d2-5822f372db"); // The last one is Client ID
+let temperature,dustLevel
+var humidity = 50 
+
+client = new Paho.MQTT.Client("mqtt.netpie.io",443,"xxx"); // The last one is Client ID
 client.onMessageArrived = onMessageArrived;
 
 var options = {
     useSSL: true,
-    userName: "MFkDHUkm86b5PBQ6x8Ejazq4cVNWnQ", // This is token
-    password: "WM8Zs6ozJGzsJYxFc1WUkPRquvQRMc", // This is secret
+    userName: "xxx", // This is token
+    password: "xxx", // This is secret
     onSuccess: onConnect,
     onFailure: doFail,                                                  
 }
 
 client.connect(options);
 
-//have to both send msg and update the shadow
-
 function onConnect(){
     client.subscribe("@msg/test");
+}
+
+function doFail(e){
+    console.log(e);
 }
 
 async function getShadowData() {
@@ -49,44 +54,53 @@ async function getShadowData() {
   sensors = fdata;
 }
 
-// getShadowData();
-// refresh(); 
-
-function refresh(){
-    getShadowData();
-    console.log(sensors);
-    document.getElementById("temp").innerHTML = "temp = " + sensors.data.temp;
-    document.getElementById("humi").innerHTML = "humi = " + sensors.data.humi;
-    document.getElementById("dust").innerHTML = "dust = " + sensors.data.dust;
-}
-
-function doFail(e){
-    console.log(e);
-}
-
 function myFunction()
 {
     // เป็นปุ่มที่ใช้เปิดปิดน้ำ
     // x = document.getElementById("myText").value ;
     // document.getElementById.innerHTML = x ;
-    if(on == "off"){
-        on = "on";
-    }else{
-        on = "off";
-    }
-    mqttSend("@msg/on",on);
-    console.log(on);
-}
+    // if(on == "off"){
+    //     on = "on";
+    //     document.getElementById("my-button").innerHTML = "On"
+    // }else{
+    //     on = "off";
+    //     document.getElementById("my-button").innerHTML = "Off"
+    // }
+    // mqttSend("@msg/on",on);
+    // console.log(on);
+    var button = document.getElementById("my-button");
+    var block = document.getElementById("button-data-block");
+            if (button.innerHTML === "off") {
+                button.innerHTML = "on";
+                button.classList.add("on"); // เพิ่มคลาส 'on' เพื่อเปลี่ยนสีเป็นสีเขียว
+                mqttSend("@msg/on", "on");
+            } else {
+                button.innerHTML = "off";
+                button.classList.remove("on"); // ลบคลาส 'on' เพื่อกลับไปสีแดง
+                mqttSend("@msg/on", "off");
+            }
+            console.log(button.innerHTML);
+    onMessageArrived("")
+}  
 
 function onMessageArrived(message)
 {
     // ใช้ array ขนาด 3 ช่อง test ก่อนก็ได้นะแบบเสมือนรับข้อมูลจาก Sensor มา ยังไม่ต้องส่งข้อมูลจริงๆ
     // เราน่าจะรับมาเป็น String รูปแบบเป็น temp,humi,dust ละเอามา split ทำเป็น array ขนาด 3 ช่อง
+
+    // ถ้าจะเทสการรับ message ให้เปิดตรงนี้
     document.getElementById("show").innerHTML = message.payloadString;
-    var temp = message.payloadString.split(",");
-    document.getElementById("temp").innerHTML = "temp = " + temp[0];
-    document.getElementById("humi").innerHTML = "humi = " + temp[1];
-    document.getElementById("dust").innerHTML = "dust = "+ temp[2];
+    var data = message.payloadString.split(",");
+    // var data = [];
+    // for (var i = 0; i < 3; i++) {
+    //     // Generate random number between 0 and 100
+    //     var randomNumber = Math.floor(Math.random() * 101);
+    //     data.push(randomNumber);
+    // }
+
+    document.getElementById("temp").innerHTML = data[0] + " &deg;C";
+    document.getElementById("humi").innerHTML = data[1] + " %";
+    document.getElementById("dust").innerHTML = data[2] + " %";
 }
 
 var mqttSend = function (topic,msg){
@@ -97,7 +111,6 @@ var mqttSend = function (topic,msg){
 
 
 
-/* */
 
 
 
